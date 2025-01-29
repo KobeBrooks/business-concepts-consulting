5ea30eabc8c6339b162e9ece0f9df0966fbc1e78
KobeBrooks <55289738+KobeBrooks@users.noreply.github.com>
2025-01-28 21:37:43 -0500

    remove basePAth

--- business-concepts-consulting/next.config.mjs
+++ business-concepts-consulting/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/business-concepts-consulting",
     output: "export", 
+    images: {
+        unoptimized: true,
+    },
     reactStrictMode: true,
   };
   
--- business-concepts-consulting/src/utils/imagePath.js
+++ business-concepts-consulting/src/utils/imagePath.js
@@ -1,5 +1,6 @@
 export const getImagePath = (src) => {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/business-concepts-consulting';
+//   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/business-concepts-consulting';
+  const basePath = '';
   console.log('basePath', basePath);
   console.log('src', src);
   return `${basePath}${src}`;
