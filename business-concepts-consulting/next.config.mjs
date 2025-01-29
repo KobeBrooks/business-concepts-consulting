1e35752d93082d1f07971fe872fd954a6d61face
KobeBrooks <55289738+KobeBrooks@users.noreply.github.com>
2025-01-28 21:26:07 -0500

    deploy some logging

--- business-concepts-consulting/next.config.mjs
+++ business-concepts-consulting/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", 
+    basePath: "/business-concepts-consulting",
+    // output: "export", 
     reactStrictMode: true,
   };
   
--- business-concepts-consulting/src/utils/imagePath.js
+++ business-concepts-consulting/src/utils/imagePath.js
@@ -1,4 +1,6 @@
 export const getImagePath = (src) => {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/business-concepts-consulting';
+  console.log('basePath', basePath);
+  console.log('src', src);
   return `${basePath}${src}`;
 }; 
