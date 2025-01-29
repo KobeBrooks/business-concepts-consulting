9c1cdc17d769e7cb068e9905179aa806a3dd24e6
KobeBrooks <55289738+KobeBrooks@users.noreply.github.com>
2025-01-28 21:42:35 -0500

    Update imagePath.js

--- business-concepts-consulting/src/utils/imagePath.js
+++ business-concepts-consulting/src/utils/imagePath.js
@@ -1,6 +1,7 @@
 export const getImagePath = (src) => {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || './business-concepts-consulting';
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/business-concepts-consulting';
   console.log('basePath', basePath);
   console.log('src', src);
+  console.log(`${basePath}${src}`);
   return `${basePath}${src}`;
 }; 
